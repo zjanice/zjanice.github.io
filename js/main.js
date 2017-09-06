cb0b5b97a70b262bc2acda30497c6d466bfc4bf6
uranusbean <uranusbean@gmail.com>
2017-09-05 20:27:01 -0400

    update project page

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:2,
+              nav:true
+          },
+          600:{
+              items:2,
+              nav:false
+          },
+          1000:{
+              items:2,
+              nav:true,
+              loop:false
+          }
+      }
+  })
+});
